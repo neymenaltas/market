5d349fc86f465f8fde55a594502077ed00524a37
neymenaltas <nurullahaltas93@gmail.com>
2025-09-22 21:44:48 +0300

    Initial Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode} from '@angular/core';
-import {provideRouter, withHashLocation} from '@angular/router';
+import { provideRouter } from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -15,10 +15,10 @@ import { provideServiceWorker } from '@angular/service-worker';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
-    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
